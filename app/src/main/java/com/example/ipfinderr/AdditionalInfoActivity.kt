dbe2b0a06715f0f1f3f4c9d86cca3432a555f2ba
Aras432 <artem.a.baranov@yandex.ru>
2024-03-27 16:13:13 +0300

    верстка

---
+++ app/src/main/java/com/example/ipfinderr/AdditionalInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ipfinderr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdditionalInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_additional_info)
+    }
+}
