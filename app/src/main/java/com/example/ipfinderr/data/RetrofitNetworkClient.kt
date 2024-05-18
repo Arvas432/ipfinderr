ad452b2b0cbdfa21e2d1e629cdeb073e158b5742
Artem <artem.a.baranov@yandex.ru>
2024-05-18 17:07:02 +0300

    мелкие фиксы

--- app/src/main/java/com/example/ipfinderr/data/RetrofitNetworkClient.kt
+++ app/src/main/java/com/example/ipfinderr/data/RetrofitNetworkClient.kt
@@ -7,6 +7,7 @@ class RetrofitNetworkClient(private val ipFindService: IpFindApi): NetworkClient
     override fun doRequest(dto: Any): Response {
         if(dto is IPFindRequest){
             val resp = ipFindService.search(dto.expression).execute()
+            Log.i("NETWORK", resp.body().toString())
             val body = resp.body() ?: Response()
             if (resp.body()!=null){
                 return IPFindResponse(resp.body()!!).apply { resultCode=resp.code() }
